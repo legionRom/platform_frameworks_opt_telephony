eafede10ae63dcedbb45eaae29b7208dc5120b8f
rajkale99 <kaleraj.rk@gmail.com>
2020-05-08 20:49:35 +0530

    gsgg

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -185,7 +185,7 @@ public class PhoneSwitcher extends Handler {
 
     // The phone ID that has an active voice call. If set, and its mobile data setting is on,
     // it will become the mPreferredDataPhoneId.
-    private int mPhoneIdInVoiceCall = SubscriptionManager.INVALID_PHONE_INDEX;
+    protected int mPhoneIdInVoiceCall = SubscriptionManager.INVALID_PHONE_INDEX;
 
     @VisibleForTesting
     // It decides:
@@ -1252,7 +1252,11 @@ public class PhoneSwitcher extends Handler {
                 subId, needValidation ? 1 : 0, callback).sendToTarget();
     }
 
+<<<<<<< HEAD
     private boolean isPhoneInVoiceCall(Phone phone) {
+=======
+    protected boolean isCallActive(Phone phone) {
+>>>>>>> 45a2812df... Add support for data call continuity during calls
         if (phone == null) {
             return false;
         }
@@ -1298,7 +1302,7 @@ public class PhoneSwitcher extends Handler {
     /**
      * See {@link PhoneStateListener#LISTEN_ACTIVE_DATA_SUBSCRIPTION_ID_CHANGE}.
      */
-    private void notifyPreferredDataSubIdChanged() {
+    protected void notifyPreferredDataSubIdChanged() {
         ITelephonyRegistry tr = ITelephonyRegistry.Stub.asInterface(ServiceManager.getService(
                 "telephony.registry"));
         try {
